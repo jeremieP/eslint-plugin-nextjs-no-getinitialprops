e55c8fc00499fcf456734d72d471cb904dd5660f
Jeremie Pereira <jeremie.pereira@adevinta.com>
2025-06-19 11:34:29 +0200

    chore: add flat recommended config

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 import noGetInitialProps from './lib/rules/nextjs-no-getinitialprops.js'
+import fs from 'fs'
 
+
+const pkg = JSON.parse(fs.readFileSync(new URL('../package.json', import.meta.url), 'utf8'))
 const rules = {
-  rules: {
-    'nextjs-no-getinitialprops': noGetInitialProps,
+  'nextjs-no-getinitialprops': noGetInitialProps,
+}
+
+const plugin = {
+  meta: {
+    name: pkg.name,
+    version: pkg.version,
+  },
+  rules,
+  configs: {
+    recommended: [
+      {
+        plugins: { [pkg.name]: plugin },
+        rules: {
+          [`${pkg.name}/nextjs-no-getinitialprops`]: 'error',
+        },
+      }
+    ],
   },
 }
 
-export default rules
+export default plugin
